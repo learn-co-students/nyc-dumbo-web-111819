b2007c7cd5526ce91a31b620cd757f74f5577e99
Sylwia Vargas <sylwia.vargas@gmail.com>
2019-12-01 17:05:46 -0500

    starter kit done

--- 10-hashes-and-the-internet/starter-kit/db/seeds.rb
+++ 10-hashes-and-the-internet/starter-kit/db/seeds.rb
@@ -1,10 +1,10 @@
 User.destroy_all
 Book.destroy_all
 
-u1 = User.create(username: "ian")
+u1 = User.create(username: "Mazen")
 
-b1 = Book.create(title: "Mastering Mod 1", authors: "Graham Troyer-Joy")
+j1 = Joke.create(content: "This is a test joke")
 
 # << will attempt to create and save a new instance using the join model...
 # if the associations are set up correctly
-u1.books << b1
+u1.jokes << j1
