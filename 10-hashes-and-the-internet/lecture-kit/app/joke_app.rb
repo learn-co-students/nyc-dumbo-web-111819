3298614fd388c4dc3619a775a0c0b9875475967a
sylwiavargas <sylwia.vargas@gmail.com>
2019-12-02 17:36:37 -0500

    done

--- 10-hashes-and-the-internet/lecture-kit/app/joke_app.rb
+++ 10-hashes-and-the-internet/lecture-kit/app/joke_app.rb
@@ -5,8 +5,7 @@ class JokeApp
   def run
     welcome
     login_or_signup
-    # wanna_see_favs?
-    # get_joke(what_subject)
+    get_joke(what_subject)
   end
 
   private
@@ -27,6 +26,47 @@ class JokeApp
     sleep(1)
     puts "Alrighty, #{@user.username.capitalize}!"
     # binding.pry
+  end
+
+  def get_joke(subject)
+    url = "https://icanhazdadjoke.com/" #random
+    search_term = "search?term=#{subject}" #specific
+
+    if subject.downcase == "random"
+      uri = URI.parse(url)
+    else
+      uri = URI.parse(url + search_term)
+    end
+
+    request = Net::HTTP::Get.new(uri)
+    request["Accept"] = "text/plain"
+    
+    req_options = {
+      use_ssl: uri.scheme == "https",
+    }
+    
+    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      http.request(request)
+    end
+
+    # binding.pry
+
+    joke = response.body.split(/(?<!\r)\n/)
+    
+    # there was no jokes?!?!!
+    if joke == nil || joke.count == 0 
+      puts "no jokes about it!"
+    else 
+      puts joke.sample.to_s.force_encoding("UTF-8")
+    end
+    
+  end
+
+  def what_subject
+    puts "Joke topic of choosing?"
+    sleep(0.5)
+    puts "Write 'random' if you want a surprise."
+    return answer = gets.chomp
   end
 
 end 
